c8f5ff97e927e0ad999bb93f800765e39d122e0c
Rizal Kholif <rizalhellboy@gmail.com>
2018-02-16 07:42:49 +0700

    First

---
+++ game1/Assets/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] sampah;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randSampah;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(waitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+        while(!stop)
+        {
+            randSampah = Random.Range(0, 2);
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+            Instantiate (sampah[randSampah], spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
---
+++ game1/Assets/TongController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TongController : MonoBehaviour {
+
+	public float speed;
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			goleft ();
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			goright ();
+		}
+		/*if(Input.GetKey(KeyCode.UpArrow)){
+			goford ();
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			goback ();
+		}*/
+
+	}
+
+	//===========================================================
+
+	private void goleft(){
+		transform.Translate (Vector3.left * speed * Time.deltaTime);
+	}
+
+	private void goright ()
+	{
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
+	}
+
+	private void goford ()
+	{
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	private void goback ()
+	{
+		transform.Translate (Vector3.back * speed * Time.deltaTime);
+	}
+}
